e5a601fe10898fe79305e92dab84b15aac2f08df
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2024-02-04 16:33:13 +0530

    Ignoring .ugit folder when adding files to the object database and editing README

--- ugit/base.py
+++ ugit/base.py
@@ -7,12 +7,19 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = os.path.join(directory, entry.name)
+            if is_ignored(full):
+                continue
 
             if entry.is_file(follow_symlinks=False):
-                #Todo write the file to object store
-                print(full)
+                #Write the file to object store
+                with open(full, 'rb') as f:
+                    print(data.hash_object(f.read()), full)
             
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
 
     # TODO actually create the tree object
+
+
+def is_ignored(path):
+    return '.ugit' in os.path.split(path)
